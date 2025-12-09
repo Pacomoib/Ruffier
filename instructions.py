c28135b791109ac773cc49492f4487822e6a3fc1
Pacomoib <paco201989@gmail.com>
2025-12-08 18:47:04 -0600

    Add files via upload

--- instructions.py
+++ instructions.py
@@ -1,34 +1,17 @@
 txt_instruction = '''
-
-Esta app nos permite \nrealizar un diagnóstico inicial de tu salud \n usando un test de Ruffier.
-
-Un test de Ruffier es una prueba de estrés\n realizada para evaluar la eficiencia del corazón durante esfuerzo físico.\n
-
-El usuario calcula su frecuencia de pulso por 15 segundos.\n
-
-Luego el usuario hace 30 sentadillas durante 45 segundos.\n
-
-Después del final del esfuerzo, el pulso es calculado de nuevo: \n el número de pulsaciones después de los primeros 15 segundos, 30 segundos de descanso \n y el número de pulsaciones en los últimos 15 segundos.\n'''
-
-
+Esta aplicación le permitiría usar el test de Ruffier \n para realizar un diagnóstico primario de su salud.\n
+El test de Ruffier es una serie de ejercicios \n destinados a evaluar el rendimiento cardíaco durante el ejercicio físico.\n
+La frecuencia cardíaca de la persona evaluada es contada por 15 segundos.\n
+Luego, en menos de 45 segundos, la persona evaluada debe realizar 30 sentadillas.\n
+Después de que termine el ejercicio físico, el pulso se vuelve a contar: \n el número de pulsaciones por los primeros 15 segundos; luego, 30 segundos de descanso;\n luego, el número de pulsaciones por los últimos 15 segundos.\n'''
 txt_test1 = '''Mide tu pulso por 15 segundos.\n
-
-Escribe tus resultados en los campos correspondientes.'''
-
-
+Escribe tu resultado en el campo correspondiente.'''
 txt_test2 = '''Realiza 30 sentadillas en 45 segundos.\n
-
-Presiona el botón “Iniciar” para ejecutar el contador de sentadillas.
-
+Presiona el botón “Iniciar” para ejecutar el contador de sentadillas.\n
 Haz las sentadillas a la velocidad del contador.'''
-
-
 txt_test3 = '''Mide tu pulso dos veces en el próximo minuto:\n
-
 durante los primeros 15 segundos y luego durante los últimos 15 segundos del minuto.\n
-
 Escribe tus resultados en los campos correspondientes.'''
 
 
 txt_sits = 'Realiza 30 sentadillas en 45 segundos.'
-
